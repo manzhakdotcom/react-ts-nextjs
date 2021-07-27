2017eca8c9f9a424d8f265ad01e4b4d457627c5b
Manzhak <manzhak.com@gmail.com>
2021-07-27 13:57:03 +0300

    add Tag components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Button, Htag } from '../components';
+import { Button, Htag, P, Tag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag='h3'>Теск</Htag>
-      <Button appearance='primary' arrow='down'>Кнопка</Button>
-      <Button appearance='ghost'>Кнопка</Button>
+      <Button appearance='primary' arrow='right'>
+        Кнопка
+      </Button>
+      <Button appearance='ghost' arrow='down'>
+        Кнопка
+      </Button>
+      <P size='l'>Большой</P>
+      <P>Средний</P>
+      <P size='s'>Маленький</P>
+      <Tag size='s'>Ghost</Tag>
+      <Tag size='m' color='red'>
+        Red
+      </Tag>
+      <Tag size='s' color='green'>
+        Green
+      </Tag>
+      <Tag color='primary'>Green</Tag>
     </>
   );
 }
